8c89e09f4b31b9b6e6f3d6a8feca9f252332662c
rdevcharles <rondell.charles@generalassemb.ly>
2023-09-11 10:39:20 -0400

    dry: team.js

--- app/components/Team.js
+++ app/components/Team.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+
+const team = [
+  { name: "John Doe", title: "Founder & CEO", image: "/person1.jpg" },
+  { name: "Jane Smith", title: "Head of Data Science", image: "/person2.jpg" },
+  { name: "Mike Johnson", title: "Lead AI Engineer", image: "/person3.jpg" },
+  { name: "Sarah Brown", title: "Data Analyst", image: "/person4.jpg" },
+];
 
 const Team = () => {
-  //TODO: DRY up the code by using a map function to iterate over the team members
+
   return (
-    <section id="team"  className="py-20 bg-gradient-to-b from-purple-300 to-magenta-400">
+    <section
+      id="team"
+      className="py-20 bg-gradient-to-b from-purple-300 to-magenta-400"
+    >
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold text-purple-800 mb-12">Our Team</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {/* Team Member 1 */}
-          <div className=" p-6 ">
-           
-            <Image className="w-32 h-32 mx-auto rounded-full mb-4 object-cover" src={"/person1.jpg"} width={150} height={150} />
-            <h3 className="text-xl font-semibold mb-2">John Doe</h3>
-            <p className="text-gray-700">Founder & CEO</p>
-          </div>
 
-          {/* Team Member 2 */}
-          <div className=" p-6 ">
-          <Image className="w-32 h-32 mx-auto rounded-full mb-4 object-cover" src={"/person2.jpg"} width={150} height={150} />
-            <h3 className="text-xl font-semibold mb-2">Jane Smith</h3>
-            <p className="text-gray-700">Head of Data Science</p>
-            
-          </div>
-
-          {/* Team Member 3 */}
-          <div className=" p-6 ">
-          <Image className="w-32 h-32 mx-auto rounded-full mb-4 object-cover" src={"/person3.jpg"} width={150} height={150} />
-            <h3 className="text-xl font-semibold mb-2">Mike Johnson</h3>
-            <p className="text-gray-700">Lead AI Engineer</p>
-            
-          </div>
-
-          {/* Team Member 4 */}
-          <div className=" p-6 ">
-          <Image className="w-32 h-32 mx-auto rounded-full mb-4 object-cover" src={"/person4.jpg"} width={150} height={150} />
-            <h3 className="text-xl font-semibold mb-2">Sarah Brown</h3>
-            <p className="text-gray-700">Data Analyst</p>
-            
-          </div>
+          {/* map over team array and display each member */}
+          
+          {team.map((member) => (
+            <div className=" p-6 ">
+              <Image
+                className="w-32 h-32 mx-auto rounded-full mb-4 object-cover"
+                src={member.image}
+                width={150}
+                height={150}
+              />
+              <h3 className="text-xl font-semibold mb-2">{member.name}</h3>
+              <p className="text-gray-700">{member.title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
