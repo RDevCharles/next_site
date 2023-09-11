ced17b0be55eed36df354cc771ede929b0a8ef98
rdevcharles <rondell.charles@generalassemb.ly>
2023-09-11 11:13:54 -0400

    smooth scroll added

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,8 @@
 "use client"
 import React from 'react';
-import Link from 'next/link';
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
 
 
 export default function Header() {
@@ -10,9 +12,9 @@ export default function Header() {
       <div className="logo">Your Logo</div>
       <nav>
         <ul>
-          <li><Link href="#services">Services</Link></li>
-          <li><Link href="#team">Team</Link></li>
-          <li><Link href="#contact">Contact</Link></li>
+          <li><Link to="services" smooth={true}>Services</Link></li>
+          <li><Link to="team" smooth={true}>Team</Link></li>
+          <li><Link to="contact" smooth={true}>Contact</Link></li>
         </ul>
       </nav>
     </header>
--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
       <Header />
       <Hero />
       <About/>
-      <Services />
+      <Services name="services" />
       <Team />
       <Contact />
       <Footer/>
